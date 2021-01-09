420dd1e395c0313d60f17a2440fd913c7184388e
Lei <ratrateroo@gmail.com>
2021-01-09 22:51:13 +0800

    saved photos to the file system

--- photo-gallery/src/hooks/usePhotoGallery.ts
+++ photo-gallery/src/hooks/usePhotoGallery.ts
@@ -17,6 +17,7 @@ export interface Photo {
 }
 
 const usePhotoGallery = () => {
+	const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
 	const { getPhoto } = useCamera();
 
 	const [photos, setPhotos] = useState<Photo[]>([]);
@@ -29,14 +30,28 @@ const usePhotoGallery = () => {
 		});
 
 		const fileName = new Date().getTime() + '.jpeg';
-		const newPhotos = [
-			{
-				filepath: fileName,
-				webviewPath: cameraPhoto.webPath,
-			},
-			...photos,
-		];
+		const savedFileImage = await savePicture(cameraPhoto, fileName);
+		const newPhotos = [savedFileImage, ...photos];
 		setPhotos(newPhotos);
+	};
+
+	const savePicture = async (
+		photo: CameraPhoto,
+		fileName: string
+	): Promise<Photo> => {
+		const base64Data = await base64FromPath(photo.webPath!);
+		const savedFile = await writeFile({
+			path: fileName,
+			data: base64Data,
+			directory: FilesystemDirectory.Data,
+		});
+
+		// Use webPath to display the new image instead of base64 since it's
+		// already loaded into memory
+		return {
+			filepath: fileName,
+			webviewPath: photo.webPath,
+		};
 	};
 
 	return {
